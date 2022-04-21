cbf190eca193f59e179edbb4e0e43c5e9cb72d14
dinesh <chavandinesh53@gmail.com>
2022-04-21 10:55:56 +0530

    added changes in ui

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+         <hr/>
+         <div className="footer__icons" style={{width: "100%", padding: "10px"}}>
+            <ul>
+
+            </ul>
+         </div>
+    </div>
+  )
+}
+
+export default Footer
