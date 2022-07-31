11ec4c867b1a9a65ae0b77e0ca9da6afb207be53
saipavan <saipavanthadem@gmail.com>
2022-07-31 23:28:21 +0530

    Updates

---
+++ precache-manifest.b5bb7df51388d7e6bf303b02392ae678.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f634796f881f23958217ae292572de3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def7e337b9e3a1e09edd",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "c3d84a40b07d47bfb529",
+    "url": "/static/css/main.a3d8a704.chunk.css"
+  },
+  {
+    "revision": "def7e337b9e3a1e09edd",
+    "url": "/static/js/2.fba206ad.chunk.js"
+  },
+  {
+    "revision": "74880a4d801e0f82b99fcd897dfb2e30",
+    "url": "/static/js/2.fba206ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d84a40b07d47bfb529",
+    "url": "/static/js/main.85abe784.chunk.js"
+  },
+  {
+    "revision": "e2895b132fe29c21fa8b",
+    "url": "/static/js/runtime-main.2479da37.js"
+  },
+  {
+    "revision": "7ebd32176327763fe1d578cb0252f8e7",
+    "url": "/static/media/aboutme.7ebd3217.png"
+  },
+  {
+    "revision": "e511b35b8e73cc9ad78ef9d6706fef48",
+    "url": "/static/media/pavanlatest.e511b35b.jpeg"
+  },
+  {
+    "revision": "8e8218a6336186ea4171052252ff2e31",
+    "url": "/static/media/saipavan.8e8218a6.pdf"
+  },
+  {
+    "revision": "e324d3000236bc43b95fd0d1d88ec37f",
+    "url": "/static/media/shake.e324d300.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13d6c6c1ad0cd42221f1775a225cd379.js"
+  "/precache-manifest.b5bb7df51388d7e6bf303b02392ae678.js"
 );
 
 self.addEventListener('message', (event) => {
